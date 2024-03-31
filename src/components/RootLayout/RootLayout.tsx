d5107e2add7597de7b1df9f55e7e90d06d0e49f0
prajnab <prajna.b18@gmail.com>
2024-03-31 23:27:20 +0530

    - Minor changes

--- src/components/RootLayout/RootLayout.tsx
+++ src/components/RootLayout/RootLayout.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 
+import "./rootLayout.css";
 import Navbar from "../Navbar/Navbar";
 import Sidebar from "../Sidebar/Sidebar";
 import { RootLayoutContextType } from "../../types";
@@ -10,25 +11,40 @@ const RootLayout = () => {
     const [sidebar, setSidebar] = useState(true);
     const [categoryId, setCategoryId] = useState<string>("0");
 
+    const overlay = useMemo(
+        () => location.pathname !== "/",
+        [location.pathname]
+    );
+
     useEffect(() => {
-        setSidebar(location.pathname !== "/" ? false : true);
-    }, [location.pathname]);
+        setSidebar(overlay ? false : true);
+    }, [overlay]);
 
     return (
         <>
             <Navbar setSidebar={setSidebar} />
             <div className="main-container">
                 <Sidebar
-                    overlay={location.pathname !== "/"}
+                    overlay={overlay}
                     sidebar={sidebar}
                     categoryId={categoryId}
                     setCategoryId={setCategoryId}
+                    setSidebar={setSidebar}
                 />
-                <Outlet
-                    context={
-                        { sidebar, categoryId } satisfies RootLayoutContextType
-                    }
-                />
+                <div
+                    className={`main-container ${
+                        overlay && sidebar ? "lock-scrollbar" : ""
+                    }`}
+                >
+                    <Outlet
+                        context={
+                            {
+                                sidebar,
+                                categoryId,
+                            } satisfies RootLayoutContextType
+                        }
+                    />
+                </div>
             </div>
         </>
     );
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
 import "./sidebar.css";
 import home from "../../assets/home.png";
 import game_icon from "../../assets/game_icon.png";
@@ -20,6 +22,7 @@ type SidebarProps = {
     sidebar: boolean;
     categoryId?: string;
     setCategoryId: React.Dispatch<React.SetStateAction<string>>;
+    setSidebar: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 const Sidebar = ({
@@ -27,125 +30,137 @@ const Sidebar = ({
     sidebar,
     categoryId,
     setCategoryId,
+    setSidebar,
 }: SidebarProps) => {
+    const navigate = useNavigate();
     const containerClasses = overlay
         ? sidebar
-            ? "sidebar-overlay"
+            ? "sidebar sidebar-overlay"
             : "sidebar-hidden"
         : sidebar
         ? "sidebar"
         : "sidebar small-sidebar";
 
+    const handleCategoryClick = (id: string) => {
+        setCategoryId(id);
+        navigate("/");
+    };
+
     return (
-        <div className={containerClasses}>
-            <div className="shortcut-links">
-                <div
-                    className={`side-link ${
-                        categoryId === "0" ? "active" : ""
-                    }`}
-                    onClick={() => setCategoryId("0")}
-                >
-                    <img src={home} alt="" />
-                    <p>Home</p>
+        <>
+            {overlay && sidebar ? (
+                <div className="overlay" onClick={() => setSidebar(false)} />
+            ) : null}
+            <div className={containerClasses}>
+                <div className="shortcut-links">
+                    <div
+                        className={`side-link ${
+                            categoryId === "0" ? "active" : ""
+                        }`}
+                        onClick={() => handleCategoryClick("0")}
+                    >
+                        <img src={home} alt="" />
+                        <p>Home</p>
+                    </div>
+                    <div
+                        className={`side-link ${
+                            categoryId === "20" ? "active" : ""
+                        }`}
+                        onClick={() => handleCategoryClick("20")}
+                    >
+                        <img src={game_icon} alt="" />
+                        <p>Gaming</p>
+                    </div>
+                    <div
+                        className={`side-link ${
+                            categoryId === "2" ? "active" : ""
+                        }`}
+                        onClick={() => handleCategoryClick("2")}
+                    >
+                        <img src={automobiles} alt="" />
+                        <p>Automobiles</p>
+                    </div>
+                    <div
+                        className={`side-link ${
+                            categoryId === "17" ? "active" : ""
+                        }`}
+                        onClick={() => handleCategoryClick("17")}
+                    >
+                        <img src={sports} alt="" />
+                        <p>Sports</p>
+                    </div>
+                    <div
+                        className={`side-link ${
+                            categoryId === "24" ? "active" : ""
+                        }`}
+                        onClick={() => handleCategoryClick("24")}
+                    >
+                        <img src={entertainment} alt="" />
+                        <p>Entertainment</p>
+                    </div>
+                    <div
+                        className={`side-link ${
+                            categoryId === "28" ? "active" : ""
+                        }`}
+                        onClick={() => handleCategoryClick("28")}
+                    >
+                        <img src={tech} alt="" />
+                        <p>Technology</p>
+                    </div>
+                    <div
+                        className={`side-link ${
+                            categoryId === "10" ? "active" : ""
+                        }`}
+                        onClick={() => handleCategoryClick("10")}
+                    >
+                        <img src={music} alt="" />
+                        <p>Music</p>
+                    </div>
+                    <div
+                        className={`side-link ${
+                            categoryId === "22" ? "active" : ""
+                        }`}
+                        onClick={() => handleCategoryClick("22")}
+                    >
+                        <img src={blogs} alt="" />
+                        <p>Blogs</p>
+                    </div>
+                    <div
+                        className={`side-link ${
+                            categoryId === "25" ? "active" : ""
+                        }`}
+                        onClick={() => handleCategoryClick("25")}
+                    >
+                        <img src={news} alt="" />
+                        <p>News</p>
+                    </div>
                 </div>
-                <div
-                    className={`side-link ${
-                        categoryId === "20" ? "active" : ""
-                    }`}
-                    onClick={() => setCategoryId("20")}
-                >
-                    <img src={game_icon} alt="" />
-                    <p>Gaming</p>
-                </div>
-                <div
-                    className={`side-link ${
-                        categoryId === "2" ? "active" : ""
-                    }`}
-                    onClick={() => setCategoryId("2")}
-                >
-                    <img src={automobiles} alt="" />
-                    <p>Automobiles</p>
-                </div>
-                <div
-                    className={`side-link ${
-                        categoryId === "17" ? "active" : ""
-                    }`}
-                    onClick={() => setCategoryId("17")}
-                >
-                    <img src={sports} alt="" />
-                    <p>Sports</p>
-                </div>
-                <div
-                    className={`side-link ${
-                        categoryId === "24" ? "active" : ""
-                    }`}
-                    onClick={() => setCategoryId("24")}
-                >
-                    <img src={entertainment} alt="" />
-                    <p>Entertainment</p>
-                </div>
-                <div
-                    className={`side-link ${
-                        categoryId === "28" ? "active" : ""
-                    }`}
-                    onClick={() => setCategoryId("28")}
-                >
-                    <img src={tech} alt="" />
-                    <p>Technology</p>
-                </div>
-                <div
-                    className={`side-link ${
-                        categoryId === "10" ? "active" : ""
-                    }`}
-                    onClick={() => setCategoryId("10")}
-                >
-                    <img src={music} alt="" />
-                    <p>Music</p>
-                </div>
-                <div
-                    className={`side-link ${
-                        categoryId === "22" ? "active" : ""
-                    }`}
-                    onClick={() => setCategoryId("22")}
-                >
-                    <img src={blogs} alt="" />
-                    <p>Blogs</p>
-                </div>
-                <div
-                    className={`side-link ${
-                        categoryId === "25" ? "active" : ""
-                    }`}
-                    onClick={() => setCategoryId("25")}
-                >
-                    <img src={news} alt="" />
-                    <p>News</p>
+                <hr />
+                <div className="subscribed-list">
+                    <h3>Subscribed</h3>
+                    <div className={"side-link"}>
+                        <img src={jack} alt="" />
+                        <p>PewDiePie</p>
+                    </div>
+                    <div className={"side-link"}>
+                        <img src={simon} alt="" />
+                        <p>Mr Beast</p>
+                    </div>
+                    <div className={"side-link"}>
+                        <img src={tom} alt="" />
+                        <p>Justin Bieber</p>
+                    </div>
+                    <div className={"side-link"}>
+                        <img src={megan} alt="" />
+                        <p>5-Minute Crafts</p>
+                    </div>
+                    <div className={"side-link"}>
+                        <img src={cameron} alt="" />
+                        <p>Nas Daily</p>
+                    </div>
                 </div>
             </div>
-            <hr />
-            <div className="subscribed-list">
-                <h3>Subscribed</h3>
-                <div className={"side-link"}>
-                    <img src={jack} alt="" />
-                    <p>PewDiePie</p>
-                </div>
-                <div className={"side-link"}>
-                    <img src={simon} alt="" />
-                    <p>Mr Beast</p>
-                </div>
-                <div className={"side-link"}>
-                    <img src={tom} alt="" />
-                    <p>Justin Bieber</p>
-                </div>
-                <div className={"side-link"}>
-                    <img src={megan} alt="" />
-                    <p>5-Minute Crafts</p>
-                </div>
-                <div className={"side-link"}>
-                    <img src={cameron} alt="" />
-                    <p>Nas Daily</p>
-                </div>
-            </div>
-        </div>
+        </>
     );
 };
 
